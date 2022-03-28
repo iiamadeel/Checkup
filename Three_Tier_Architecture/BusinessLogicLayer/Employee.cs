aa16c28fcd10f60f7d3491f9b8b5c6d6579ddd4b
Muhammad Adeel <87264100+iiamadeel@users.noreply.github.com>
2022-03-28 11:50:21 +0500

    Add files via upload

---
+++ Three_Tier_Architecture/BusinessLogicLayer/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer
+{
+    public class Employee
+    {
+        int id;
+
+        public Employee(int id)
+        {
+            Id = id;
+        }
+
+        string name;
+        string city;
+        string department;
+        string gender;
+
+        public Employee(int id,string name, string city, string department, string gender) : this(name, city, department, gender)
+        {
+            Id = id;
+        }
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name, string city, string department, string gender)
+        {
+            Name = name;
+            City = city;
+            Department = department;
+            Gender = gender;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string City { get => city; set => city = value; }
+        public string Department { get => department; set => department = value; }
+        public string Gender { get => gender; set => gender = value; }
+        public int Id { get => id; set => id = value; }
+    }
+}
---
+++ Three_Tier_Architecture/DataAccessLayer/DBConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class DBConnection
+    {
+        public static string getConnection()
+        {
+            return string.Format(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Adeel Khan\source\repos\Three_Tier_Architecture\DataAccessLayer\StudentDB.mdf;Integrated Security=True");
+        }
+    }
+}
